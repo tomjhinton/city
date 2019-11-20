5b6a181822e8212d4bdc18b5f53bd512b007a9f1
Tom Hinton <tomjhinton@gmail.com>
2019-11-20 12:57:57 +0000

    inital react setup

---
+++ src/components/Main.js
@@ -0,0 +1,51 @@
+import React from 'react'
+
+
+
+
+
+
+class Main extends React.Component {
+
+
+  constructor(props) {
+    super(props)
+    this.state = {
+
+
+
+    }
+
+
+
+
+  }
+
+
+
+  componentDidMount() {
+
+
+  }
+
+
+
+
+  render(){
+    return(
+
+      <div className="container">
+
+
+      MAIN
+
+
+
+      </div>
+    )
+  }
+}
+
+
+
+export default Main
---
+++ src/components/Sidebar.js
@@ -0,0 +1,56 @@
+import React from 'react'
+
+
+
+
+
+class Sidebar extends React.Component {
+
+
+  constructor() {
+    super()
+
+    this.state = {
+      time: {}
+
+    }
+
+
+
+  }
+
+
+
+
+
+  componentDidMount() {
+
+
+
+  }
+
+
+  formatCash(n) {
+    if (n < 1e3) return n
+    if (n >= 1e3 && n < 1e6) return +(n / 1e3).toFixed(1) + 'K'
+    if (n >= 1e6 && n < 1e9) return +(n / 1e6).toFixed(1) + 'M'
+    if (n >= 1e9 && n < 1e12) return +(n / 1e9).toFixed(1) + 'B'
+    if (n >= 1e12) return +(n / 1e12).toFixed(1) + 'T'
+  }
+
+
+
+
+  render() {
+
+
+
+    return(
+      <div className='sidebar'>
+        Sidebar
+      </div>
+    )
+  }
+}
+
+export default Sidebar
